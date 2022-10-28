9212eefe411a08d073f6590733bcc7b39c571c3b
Vladimir <kuro4king@gmail.com>
2022-10-28 23:34:58 +0300

    14th lesson Added css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
+import classes from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img
                 src="https://i.pinimg.com/originals/1e/12/bb/1e12bbe85660b243513671b120edd849.png"
-                alt="Инди-заряд"/>
-
+                alt="Миньоны"/>
         </header>
     )
 }
